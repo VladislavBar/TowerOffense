9a8410d03a1c10fac7b3b6d8693c4a1a74cc8f90
Vlad B <barabolya.vlad@gmail.com>
2024-09-04 00:41:42 +0300

    feat: added TankPawn, map, adjust camera, added ActionInputs

--- Source/TowerOffense/TankPawn.cpp
+++ Source/TowerOffense/TankPawn.cpp
@@ -1,5 +1,46 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "TankPawn.h"
+#include "EnhancedInputComponent.h"
 
+ATankPawn::ATankPawn()
+{
+	SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArm"));
+	SpringArm->SetupAttachment(RootComponent);
+
+	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+	Camera->SetupAttachment(SpringArm);
+}
+
+void ATankPawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	UEnhancedInputComponent* EnhancedInputComponent = Cast<UEnhancedInputComponent>(PlayerInputComponent);
+	if (EnhancedInputComponent) return;
+
+	if (IsValid(MoveForwardAction))
+	{
+		EnhancedInputComponent->BindAction(MoveForwardAction, ETriggerEvent::Started, this, &ATankPawn::Move);
+	}
+
+	if (IsValid(TurnRightAction))
+	{
+		EnhancedInputComponent->BindAction(TurnRightAction, ETriggerEvent::Started, this, &ATankPawn::Turn);
+	}
+
+	if (IsValid(FireAction))
+	{
+		EnhancedInputComponent->BindAction(FireAction, ETriggerEvent::Triggered, this, &ATankPawn::Fire);
+	}
+}
+
+void ATankPawn::Move(const FInputActionInstance& ActionData)
+{
+}
+
+void ATankPawn::Turn(const FInputActionInstance& ActionData)
+{
+}
+
+void ATankPawn::Fire()
+{
+}
--- Source/TowerOffense/TankPawn.h
+++ Source/TowerOffense/TankPawn.h
@@ -1,17 +1,38 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
+#include "InputAction.h"
 #include "TurretPawn.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/SpringArmComponent.h"
 #include "TankPawn.generated.h"
 
-/**
- * 
- */
 UCLASS()
 class TOWEROFFENSE_API ATankPawn : public ATurretPawn
 {
 	GENERATED_BODY()
-	
+
+	UPROPERTY(EditDefaultsOnly)
+	TObjectPtr<USpringArmComponent> SpringArm;
+
+	UPROPERTY(EditDefaultsOnly)
+	TObjectPtr<UCameraComponent> Camera;
+
+	UPROPERTY(EditDefaultsOnly)
+	TObjectPtr<UInputAction> MoveForwardAction;
+
+	UPROPERTY(EditDefaultsOnly)
+	TObjectPtr<UInputAction> TurnRightAction;
+
+	UPROPERTY(EditDefaultsOnly)
+	TObjectPtr<UInputAction> FireAction;
+
+private:
+	virtual void SetupPlayerInputComponent(UInputComponent* PlayerInputComponent) override;
+	void Move(const FInputActionInstance& ActionData);
+	void Turn(const FInputActionInstance& ActionData);
+	void Fire();
+
+public:
+	ATankPawn();
 };
